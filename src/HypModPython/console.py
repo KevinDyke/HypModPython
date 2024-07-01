865c818b785eb8f5aa5b3940049d6e5b57eb6ea2
Kevin James Dyke <kdyke058@gmail.com>
2024-07-01 23:03:46 +0100

    Add local language support

--- src/HypModPython/console.py
+++ src/HypModPython/console.py
@@ -1,5 +1,7 @@
-# src/HypModPython/console.py 
+""" Modern Python - Get random fact from wikipedia """
+import locale
 import textwrap
+from typing import Tuple
 
 import click
 import requests
@@ -9,23 +11,30 @@ from . import __version__
 
 API_URL: str = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
+
 @click.command()
 @click.option(
     "--language",
     "-l",
-    default="en",
+    default="local",
     help="Language edition of Wikipedia",
     metavar="LANG",
     show_default=True,
 )
 @click.version_option(version=__version__)
 def main(language: str = "en"):
-    """ The hypermodern Python project."""
+    """The hypermodern Python project."""
+
+    if language.lower() == "local":
+        lang: Tuple[str | None, str | None] = locale.getlocale(locale.LC_CTYPE)
+        if lang[0] is not None:
+            language = lang[0][:2]
+
     url = API_URL.format(language=language)
-    with requests.get(url) as response:
+    with requests.get(url, timeout=5) as response:
         try:
             response.raise_for_status()
-        
+
             if response.status_code == 200:
                 data = response.json()
 
