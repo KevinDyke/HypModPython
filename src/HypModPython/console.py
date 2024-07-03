4e38814531ac83485e5c6bc2038bd7d66d36f547
Kevin James Dyke <kdyke058@gmail.com>
2024-07-03 22:18:22 +0100

    Added more tests

--- src/HypModPython/console.py
+++ src/HypModPython/console.py
@@ -1,49 +1,26 @@
 """ Modern Python - Get random fact from wikipedia """
-import locale
 import textwrap
-from typing import Tuple
 
 import click
-import requests
-from requests.exceptions import HTTPError
 
-from . import __version__
-
-API_URL: str = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
-
+from . import __version__, wikipedia
 
 @click.command()
 @click.option(
     "--language",
     "-l",
-    default="local",
+    default="en",
     help="Language edition of Wikipedia",
     metavar="LANG",
     show_default=True,
 )
 @click.version_option(version=__version__)
-def main(language: str = "en"):
-    """The hypermodern Python project."""
-
-    if language.lower() == "local":
-        lang: Tuple[str | None, str | None] = locale.getlocale(locale.LC_CTYPE)
-        if lang[0] is not None:
-            language = lang[0][:2]
-
-    url = API_URL.format(language=language)
-    with requests.get(url, timeout=5) as response:
-        try:
-            response.raise_for_status()
-
-            if response.status_code == 200:
-                data = response.json()
-
-                title = data["title"]
-                extract = data["extract"]
-
-                click.secho(title, fg="green")
-                click.echo(textwrap.fill(extract))
-            else:
-                click.echo("Something went wrong!")
-        except HTTPError as http_err:
-            click.echo(f"HTTP error occurred: {http_err}")
+def main(language):
+    """ The hypermodern Python Python project. """
+    data = wikipedia.random_page(language=language)
+        
+    title = data["title"]
+    extract = data["extract"]
+        
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
