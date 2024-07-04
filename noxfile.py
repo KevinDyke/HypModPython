0c8adb7a178e1e3013f1538a8e6b989cb6823689
Kevin James Dyke <kdyke058@gmail.com>
2024-07-04 16:27:58 +0100

    Updated noxfile to include safety

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,10 @@
 """ nox file for hyper modern python project """
 
+import tempfile
+
 import nox
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
@@ -31,3 +33,24 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python="3.11")
+def safety(session):
+    with tempfile.NamedTemporaryFile(delete=False, dir=".",mode='wt') as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run(
+            "safety",
+            "check",
+            f"--file={requirements.name}",
+            "--full-report",
+        )
